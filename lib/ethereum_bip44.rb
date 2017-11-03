c9573bf5362009cc74b0d4c432cfb907eb708ab8
wuminzhe <wuminzhe@gmail.com>
2017-11-03 13:56:18 +0800

    update

--- lib/ethereum_bip44.rb
+++ lib/ethereum_bip44.rb
@@ -32,19 +32,29 @@ module EthereumBip44
 
 
     def get_address(index)
+      # get the public key of index
       parts = @parts[@keychain.depth..-1]
       path = 'm/' + (!parts.empty? ? parts.join('/') + '/' : '') + index.to_s
       derived = @keychain.derived_keychain(path)
       public_key_string = derived.key.public_key
+
+      # translate the public key to ethereum public key
       group = ECDSA::Group::Secp256k1
       public_key = ECDSA::Format::PointOctetString.decode(public_key_string, group) # a point
       ethereum_public = public_key.x.to_s(16) + public_key.y.to_s(16)
 
-      bytes = EthereumBip44.hex_to_bin(ethereum_public)
+      # get the address from the ethereum public key
+      bytes = Utils.hex_to_bin(ethereum_public)
       address_bytes = Digest::SHA3.new(256).digest(bytes)[-20..-1]
+      address = Utils.bin_to_hex(address_bytes)
+      Utils.prefix_hex(address)
+    end
 
-      address = EthereumBip44.bin_to_hex(address_bytes)
-      EthereumBip44.prefix_hex(address)
+    def get_private_key(index)
+      parts = @parts[@keychain.depth..-1]
+      path = 'm/' + (!parts.empty? ? parts.join('/') + '/' : '') + index.to_s
+      derived = @keychain.derived_keychain(path)
+      Utils.bin_to_hex(derived.key.private_key)
     end
   end
 end
--- lib/ethereum_bip44/utils.rb
+++ lib/ethereum_bip44/utils.rb
@@ -1,14 +1,15 @@
 module EthereumBip44
-  def self.hex_to_bin(string)
-    RLP::Utils.decode_hex string
-  end
+  module Utils
+    def self.hex_to_bin(string)
+      RLP::Utils.decode_hex string
+    end
 
-  def self.bin_to_hex(string)
-    RLP::Utils.encode_hex string
-  end
+    def self.bin_to_hex(string)
+      RLP::Utils.encode_hex string
+    end
 
-  def self.prefix_hex(hex)
-    hex.match(/\A0x/) ? hex : "0x#{hex}"
+    def self.prefix_hex(hex)
+      hex.match(/\A0x/) ? hex : "0x#{hex}"
+    end
   end
-
 end
